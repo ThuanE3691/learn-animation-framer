697717f4ff772af76c3fa871b5c7d122b2693a46
Thuan Nguyen Minh <thuan.nguyen.minh@preciofishbone.se>
2024-05-22 16:22:29 +0700

    Add particles into background of web

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,13 @@ import { useCopyToClipboard } from "react-use";
 import animationSample from "./animationSample";
 import { useState } from "react";
 import { SelectAnimation } from "./components/SelectAnimation";
+import ParticlesContainer from "./components/ParticleContainer";
 
 function App() {
 	const [animationSelected, setAnimationSelected] = useState<string>("Fade In");
 	const [state, copyToClipboard] = useCopyToClipboard();
+  const [initParticles, setInitParticles] = useState(false);
+
 
 	const copyCode = () => {
 		// Logic to copy `code`
@@ -17,50 +20,53 @@ function App() {
 	const AnimationWrap = animationSample[animationSelected].component;
 
 	return (
-		<div className="h-[1200px] relative">
-			<div className="flex flex-col items-center justify-center w-full">
-				<h1 className="text-4xl italic">Animation Framer</h1>
-				<h4 className="">By Thuận</h4>
-			</div>
-			<div className="flex mt-6 ml-6">
-				<div className="relative">
-					<CodeBlock
-						code={animationSample[animationSelected].code}
-						language="tsx"
-					>
-						<CodeBlock.Code className="p-6 bg-gray-900 shadow-lg rounded-xl w-[700px] relative">
-							<div className="table-row">
-								<CodeBlock.LineNumber className="table-cell pr-4 text-sm text-right text-gray-500 select-none" />
-								<CodeBlock.LineContent className="table-cell">
-									<CodeBlock.Token />
-								</CodeBlock.LineContent>
-							</div>
-						</CodeBlock.Code>
-						<button
-							className="bg-white rounded-full px-3.5 py-1.5 absolute top-2 right-2 text-sm font-semibold"
-							onClick={copyCode}
-						>
-							{state.value ? "Copied!" : "Copy code"}
-						</button>
-					</CodeBlock>
-				</div>
+        <div className="h-[1200px] relative">
+            <div>
+                <ParticlesContainer
+                    className={"absolute top-0 left-0 w-full h-full z-[-1]"}
+                    init={initParticles}
+                    setInit={setInitParticles}
+                ></ParticlesContainer>
+            </div>
 
-				<div className="ml-12">
-					<SelectAnimation
-						animationSelected={animationSelected}
-						setAnimation={setAnimationSelected}
-					></SelectAnimation>
-					<div className="mt-4 font-mono text-3xl italic font-bold">
-						Scroll down to see animation
-					</div>
-				</div>
-			</div>
-			<div className="w-[700px] mt-16 flex justify-center demo"></div>
-			<AnimationWrap className="flex justify-center w-full translate-y-[100px]">
-				<CardDemo></CardDemo>
-			</AnimationWrap>
-		</div>
-	);
+            <div className="flex flex-col items-center justify-center w-full">
+                <h1 className="text-4xl italic">Animation Framer</h1>
+                <h4 className="">By Thuận</h4>
+            </div>
+            <div className="flex mt-6 ml-6">
+                <div className="relative">
+                    <CodeBlock code={animationSample[animationSelected].code} language="tsx">
+                        <CodeBlock.Code className="p-6 bg-gray-900 shadow-lg rounded-xl w-[700px] relative">
+                            <div className="table-row">
+                                <CodeBlock.LineNumber className="table-cell pr-4 text-sm text-right text-gray-500 select-none" />
+                                <CodeBlock.LineContent className="table-cell">
+                                    <CodeBlock.Token />
+                                </CodeBlock.LineContent>
+                            </div>
+                        </CodeBlock.Code>
+                        <button
+                            className="bg-white rounded-full px-3.5 py-1.5 absolute top-2 right-2 text-sm font-semibold"
+                            onClick={copyCode}
+                        >
+                            {state.value ? "Copied!" : "Copy code"}
+                        </button>
+                    </CodeBlock>
+                </div>
+
+                <div className="ml-12">
+                    <SelectAnimation
+                        animationSelected={animationSelected}
+                        setAnimation={setAnimationSelected}
+                    ></SelectAnimation>
+                    <div className="mt-4 font-mono text-3xl italic font-bold">Scroll down to see animation</div>
+                </div>
+            </div>
+            <div className="w-[700px] mt-16 flex justify-center demo"></div>
+            <AnimationWrap className="flex justify-center w-full translate-y-[100px]">
+                <CardDemo></CardDemo>
+            </AnimationWrap>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/ParticleContainer.tsx
@@ -0,0 +1,69 @@
+import {useEffect, useMemo} from "react";
+import Particles, {initParticlesEngine} from "@tsparticles/react";
+import {type Container, type ISourceOptions} from "@tsparticles/engine";
+import {loadSlim} from "@tsparticles/slim"; // if you are going to use `loadSlim`, install the "@tsparticles/slim" package too.
+
+const ParticlesContainer = ({init, setInit, className}) => {
+
+    // this should be run only once per application lifetime
+    useEffect(() => {
+        initParticlesEngine(async (engine) => {
+            // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
+            // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+            // starting from v2 you can add only the features you need reducing the bundle size
+            //await loadAll(engine);
+            //await loadFull(engine);
+            await loadSlim(engine);
+            //await loadBasic(engine);
+        }).then(() => {
+            setInit(true);
+        });
+    }, []);
+
+    const particlesLoaded = async (container?: Container): Promise<void> => {
+        console.log(container);
+    };
+
+    const options: ISourceOptions = useMemo(
+        () => ({
+            background: {
+                color: "#fff",
+            },
+            styles: {
+              height: "100%",
+            },
+            particles: {
+                color: {
+                    value: "#000",
+                },
+                number: {
+                    value: 100,
+                },
+                links: {
+                    distance: 150,
+                    enable: true,
+                    color: "#000",
+                },
+                move: {
+                    enable: true,
+                },
+                size: {
+                    value: 2,
+                },
+                shape: {
+                    type: "circle",
+                },
+            },
+            fullScreen: false,
+        }),
+        []
+    );
+
+    if (init) {
+        return <Particles className={className} id="tsparticles" particlesLoaded={particlesLoaded} options={options} />;
+    }
+
+    return <></>;
+};
+
+export default ParticlesContainer;
